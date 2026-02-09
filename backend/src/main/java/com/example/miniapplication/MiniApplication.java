3d1333b860adff6a2e1f176e8a9fcf8f513311ed
Larpeeyy <maestradoralphkeane@gmail.com>
2026-02-10 02:21:20 +0800

    Update core application config and setup

--- backend/src/main/java/com/example/miniapplication/MiniApplication.java
+++ backend/src/main/java/com/example/miniapplication/MiniApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniapplicationApplication {
-
+public class MiniApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(MiniapplicationApplication.class, args);
+		SpringApplication.run(MiniApplication.class, args);
 	}
-
 }
